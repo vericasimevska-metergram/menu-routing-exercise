254f073665d73ae9ee6746b94fa5b046d2ab7e12
vericasimevska-metergram <verica.simevska@metergram.com>
2022-05-10 12:43:49 +0200

    Improved

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,24 +1,23 @@
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
-import { About } from '../About'
-import { Home } from '../Home.js'
-import { Services } from '../Services.js'
+import { Link, Route, Routes } from "react-router-dom";
+import { About } from '../../pages/About'
+import { Home } from '../../pages/Home.js'
+import { Services } from '../../pages/Services.js'
 import styles from './menu.module.css'
 
 export function Menu() {
+
     return (
         <div>
-            <Router>
-                <nav className={styles.menu}>
-                    <Link to="/">Home</Link>
-                    <Link to="/about-us">About</Link>
-                    <Link to="/services">Services</Link>
-                </nav>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about-us" element={<About />} />
-                    <Route path="/services/*" element={<Services />} />
-                </Routes>
-            </Router>
+            <nav className={styles.menu}>
+                <Link to="/">Home</Link>
+                <Link to="/about-us">About</Link>
+                <Link to="/services">Services</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about-us" element={<About />} />
+                <Route path="/services/*" element={<Services />} />
+            </Routes>
         </div>
     )
 }
